76ddc7ba1a049240a9a1a6a20f62a0912224c8b6
Zofia Szybalska <zochula111@gmail.com>
2021-03-27 20:14:58 +0100

    Fixes

--- SPOJ/SABBIRGAME/Program.cs
+++ SPOJ/SABBIRGAME/Program.cs
@@ -9,12 +9,12 @@ namespace SABBIRGAME
         {
             int numberOfCases = int.Parse(Console.ReadLine());
             for (int i = 0; i < numberOfCases; i++) {
-                BigInteger minHP = 0;
-                BigInteger currentHP = 0;
+                long minHP = 0;
+                long currentHP = 0;
                 int numberOfLevels = int.Parse(Console.ReadLine());
                 string[] HPsForLevel = Console.ReadLine().Split(" ");
                 for (int j = 0; j < numberOfLevels; j++) {
-                    currentHP += BigInteger.Parse(HPsForLevel[j]);
+                    currentHP += long.Parse(HPsForLevel[j]);
                     if (currentHP < 1)
                     {
                         minHP += 1 - currentHP;
